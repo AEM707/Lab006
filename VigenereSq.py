1350debd52514ce4f29db1d5336ab07bd92a93e3
Andy Miranda <amiranda267@mycr.redwoods.edu>
2025-03-15 19:53:41 -0700

    finished version

--- VigenereSq.py
+++ VigenereSq.py
@@ -1,57 +1,104 @@
-def vigenere_header(alphabet):
-    alpha_len =len(alphabet)
-    print('|  ', end='')
-    for i in range(alpha_len):
-        print(f"| {alphabet[i % alpha_len]}", end=' ')
-    print("|")
-    print(f"{'|---'*(alpha_len + 1)}|")
+from sys import exit
 
+def vigenere_head(alphabet):
+   return list(' ') + list(alphabet)
 
 def vigenere_sq(alphabet):
-    alpha_len = len(alphabet)
-    vigenere_header(alphabet)
-    for shift in range(alpha_len):
-        for i in range(alpha_len):
-            if i == 0:
-                c = alphabet[(i +shift) % alpha_len]
-                print(f"| {c}", end=' ')
-                print(f"| {c}", end=' ')
-            else:
-                print(f"| {alphabet[(i + shift) % alpha_len]}", end=' ')
-        print("|")
+    alphabet = list(alphabet)
+    sq_list = [vigenere_head(alphabet)]
+    for i in range(len(alphabet)):
+        sq_list.append(list(alphabet[i]) + alphabet[i:] + alphabet[:i])
+    return sq_list
 
-def letter_to_index(letter, alphabet:str):
-    return alphabet.lower().index(letter.lower())
+def vigenere_sq_print(sq_list):
+    for i, row in enumerate(sq_list):
+        print(f"| {' | '.join(row)} |")
+        if i ==0:
+            print(f'{'|---'*len(row)}|')
+
+def letter_to_index(letter, alphabet):
+    return alphabet.find(letter)
 
 def index_to_letter(index, alphabet):
     if 0 <= index < len(alphabet):
         return alphabet[index]
-    return ''
+
 
 def vigenere_index(key_letter, plaintext_letter, alphabet):
-    return (letter_to_index(key_letter, alphabet) +
-            letter_to_index(plaintext_letter, alphabet)) %  len(alphabet)
+    return index_to_letter(
+        (letter_to_index(plaintext_letter, alphabet) +
+            letter_to_index(key_letter, alphabet)) %  len(alphabet), alphabet)
+
+
+def non_vigenere_index(key_letter, cipher_text, alphabet):
+    return index_to_letter(
+        (letter_to_index(cipher_text, alphabet) -
+         letter_to_index(key_letter, alphabet)) % len(alphabet), alphabet)
+
 
 def encrypt_vigenere(key, plaintext, alphabet):
-    cipher_text = ''
+    cipher_text = []
     counter = 0
     for c in plaintext:
         if c == ' ':
-            cipher_text+=' '
-        elif(c.upper() in alphabet):
-            cipher_text += index_to_letter(vigenere_index(key[counter%len(key)], c, alphabet), alphabet)
+            cipher_text.append(' ')
+        elif c in alphabet:
+            cipher_text.append(vigenere_index(key[counter % len(key)], c, alphabet))
             counter += 1
-    return cipher_text
+    return ''.join (cipher_text)
 
-key = 'BLUESMURF'
-alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-message = 'Hello World, I am here'
+def decrypt_vigenere(key, cipher_text, alphabet):
+    plaintext = []
+    counter = 0
+    for c in cipher_text:
+        if c == ' ':
+            plaintext.append(' ')
+        elif c in alphabet:
+            plaintext.append(non_vigenere_index(key[counter % len(key)], c, alphabet))
+            counter += 1
+    return ''.join(plaintext)
+
+def enc_menu(key, alphabet, encrypted_list):
+    plaintext =input("Enter the text you'd like to encrypt: ")
+    encrypted_list.append(encrypt_vigenere(key, plaintext, alphabet))
+
+def dec_menu(key, alphabet, encrypted_list):
+    for cipher_text in encrypted_list:
+        print (decrypt_vigenere(key, cipher_text, alphabet))
+
+def dec_dump_menu(encrypted_list):
+    for cipher_text in encrypted_list:
+        print(cipher_text)
+
+def main():
+    key = 'bluesmurf'
+    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    # message = 'One small step for man, one giant leap for mankind.'
+    encrypted_list = []
+
+    menu = [
+        ['1). Encrypt', enc_menu, [key, alphabet, encrypted_list]],
+        ['2). Decrypt', dec_menu, [key, alphabet, encrypted_list]],
+        ['3). Dump Decrypt', dec_dump_menu, [encrypted_list]],
+        ['4). Quit', exit, [0]]
+    ]
+    while True:
+        print("-"*80)
+        for menu_item in menu:
+            print(menu_item[0])
+        try:
+            choice = int(input("Make your choice: "))
+            if not (0 < choice  <= len(menu)):
+                print("Improper Choice")
+            else:
+                menu[choice-1][1](*menu[choice-1][2])
+        except ValueError as ignored:
+            print("Improper Choice, you must enter an integer between 1 and 4")
+
+
+if __name__ == '__main__':
+        main()
 
 
 
-#vigenere_sq(alphabet)
-#print(letter_to_index('H', alphabet))
-#print(index_to_letter('7', alphabet))
-#print(index_to_letter(vigenere_index('B', 'H', alphabet), alphabet))
 
-print(encrypt_vigenere(key, message, alphabet))
